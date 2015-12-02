a755c9dcebab4f7ff6f90f0a1817b06b8f4e46f7
rektide <rektide@voodoowarez.com>
2015-12-02 00:43:25 -0500

    "great"

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 
 import {
-	Appear, BlockQuote, Cite, CodePane, Deck, Fill,
+	Appear, BlockQuote, Cite, CodePane, Deck, Fit, Fill,
 	Heading, Image, Layout, Link, ListItem, List, Markdown, Quote, Slide, Spectacle, Text
 } from "./src";
 
@@ -24,6 +24,8 @@ preloader([images.city, images.kat, images.markdown]);
 
 var defaulter= function(fn, opts){
 	return function(o){
+		if(!o)
+			o= arguments[0]= {}
 		for(var i in opts){
 			if( o[i]=== undefined){
 				o[i]= opts[i]
@@ -37,23 +39,49 @@ var S= React.createFactory(Slide)
 var H= React.createFactory(Heading)
 var A= React.createFactory(Appear)
 var L= React.createFactory(Link)
+var LI= React.createFactory(ListItem)
+var MD= React.createFactory(Markdown)
 var T= React.createFactory(Text)
+var layout= React.createFactory(Layout)
+var fit= React.createFactory(Fit)
+var fill= React.createFactory(Fill)
+var code= React.createFactory(CodePane)
+var br= React.createFactory("br")
+var div= React.createFactory("div")
+var span= React.createFactory("span")
+var iframe= React.createFactory("iframe")
 
-var A1= defaulter(A, {fid: 1})
-var A2= defaulter(A, {fid: 1})
-var A3= defaulter(A, {fid: 1})
-var A4= defaulter(A, {fid: 1})
+var A0= defaulter(A, {fid: 1})
+var A1= defaulter(A, {fid: 2})
+var A2= defaulter(A, {fid: 3})
+var A3= defaulter(A, {fid: 4})
+var A4= defaulter(A, {fid: 5})
+var A5= defaulter(A, {fid: 5})
+var A6= defaulter(A, {fid: 5})
+var A7= defaulter(A, {fid: 5})
+var A8= defaulter(A, {fid: 5})
+var A9= defaulter(A, {fid: 5})
+var Hb= defaulter(H, {big: true})
+var Hbf= defaulter(Hb, {fit: true})
 
 var m= 0.6 // medium
 
 var spec= "https://tools.ietf.org/html/draft-ietf-webpush-protocol-02"
+function ss(section){
+	return "https://tools.ietf.org/html/draft-ietf-webpush-protocol-02#section-" + section
+}
+function ssl(section){
+	var href= ss(section)
+	return L({href}, href)
+}
 var specSlug= "Generic Event Delivery Using HTTP Push"
-var WP= H(null,
+var WP= Hb(null,
 		L({href: spec},
 			 "WebPush"))
 var HTTP= <Heading big>HTTP2</Heading>
 
-var s1= ()=> {
+
+var webpushAway= ()=> {
 	var draft= T(null, L({href: spec}, "draft-ietf-webpush-protocol-02"))
 	return S(null, 
 		H({size:0.6}, "Hi. Welcome to"),
@@ -61,57 +89,217 @@ var s1= ()=> {
 		T(null, "This is a talk on"),
 		WP,
 		draft,
-		T(null, slug)
+		T(null, specSlug)
 	)
 }()
 
-var s2= ()=> {
+
+
+var rad= (notes)=> {
+	return S({notes},
+		A0(null,
+			H(null, "It*'s"),
+			Hb(null, "rad")),
+		A1(null,
+			T(null, "*Webpush."),
+			T(null, "But this talk is too."),
+			T(null, "But not as rad as")))
+}()
+
+var radSpec= (notes)=> {
+	return S(notes,
+		A0(null, WP),
+		A1(null,
+			T(null, specSlug)))
+}()
+
+var bigQueue= (notes)=> {
+	return S({notes},
+		A0(null,
+			H(null, "Because"),
+			WP,
+			T(null, "is a web queue")),
+		A1(null,
+			T(null, "And queues are")),
+		A2(null,
+			Hb(null, "Great")))
+}(`
+WebPush is a big queue.
+And queues are great.
+They send stuff! Put something in a queue, and get it from elsewhere.
+`)
+
+
+var series= (notes)=> {
+	// A queue is pretty close to
+	// a series of tubes
+}()
+
+// put things in
+// get things out
+
+// a queue is pretty close to
+// [this]
+
+// things in, things out
+
+// and in addition to stuff in and stuff out
+// this talk has one other awesome
+// magic
+// http2 magic!
+// please, stick around and enjoy WebPush Away!
+
+
+var webGreat= (notes)=> {
+	return S({notes},
+		A0(null, T("The web is")),
+		A1(null, Hb("Great")))
+}()
+
+var webPushGreat= (notes)=> {
+	return S({notes},
+		WP,
+		T(null, "is a great queue"),
+		T(null, "for a great"),
+		Hg(null, "Web"))
+}()
+
+
+var beLikeTubes= (notes)=> {
+	var src= ""
+	return S({notes},
+		A0(null, Hb(null, "Queues are like")),
+		A1(null,
+			T("A series of tubes"),
+			iframe({src})))
+}()
+
+var s4_really= (notes)=> {
+	return S({notes, bgColor: "black"},
+		A0(null,
+			Hb(null, "Really great")))
+}()
+
+
+// and- Webpush is a great queue
+// What is a queue?
+// Why is webpush a great queue?
+
+// Well, a queue is something you put stuff into
+// And get stuff out of
+// WebPush is great, because it's that, on the web
+
+// Ask a "push service" server
+//   for a queue
+// And it'll give it to you
+
+// See? Three pieces. In, out, and an ack (receipt)
+
+// Webpush does that
+// And it uses HTTP2
+//   in a way that makes webpush powerful
+// Webpush also 
+
+
+
+var s5= (notes)=> {
+	return S({notes},
+		layout({}, 
+			fit(null,
+				A0(null, T(null, "You put things"))),
+			fill(null,
+				A1(null, Hb(null, "IN"))),
+			fit(null,
+				A0(null, T(null, "a queue")))),
+		layout(null,
+			fit(
+				A2(null, T(null, "And later and/or elsewhere")),
+				A2(null, T(null, "You get things"))),
+			fill(null,
+				A3(null, Hb(null, "OUT"))),
+			fit(null,
+				A2(null, T(null, "of a a queue")))))
+}
+
+var receipt= (notes)=> {
+	return S(null,
+		A4(null,
+				T(null, "That's pretty great")),
+		layout({}, 
+			fit(null,
+				A5(null, T(null, "Perhaps a"))),
+			fill(null,
+				A6(null, Hb(null, "RECEIPT"))),
+			fit(null,
+				A5(null, T(null, "that a thing has been sent")))))
+}()
+
+var s6= (notes)=> {
+	return S({notes},
+		Hbf(null, "That's pretty great"))
+}()
+
+var httpGreat= (notes)=> {
+	return S({notes},
+		A0(null,
+			WP,
+			T(null, "is really great")))
+
+}()
+
+var s7= (notes)=> {
+	return S({notes},
+		A0(null, T(null, "WebPush is one way a queue might look on the net")),
+		A1(null, T(null, "It provides those three resources")),
+		A2(null, T(null, "All you have to do is <b>POST</b> to create it")))
+}()
+
+var s8= (notes)=> {
+	var source= `POST /subscribe/ HTTP/1.1
+Host: yoyodyne.net`
+	var href= ss("4")
+	return S({notes},
+		code({source, language: "http", size: 4}),
+		ssl(4))
+}()
+
+var s9_created= (notes)=> {
+	var source= `HTTP/1.1 201 Created
+Date: Thu, 11 Dec 2014 23:56:52 GMT
+Link: </p/JzLQ3raZJfFBR0aqvOMsLrt54w4rJUsV>;
+	rel="urn:ietf:params:push"
+Link: </receipts/xjTG79I3VuptNWS0DsFu4ihT97aE6UQJ>;
+	rel="urn:ietf:params:push:receipt"
+Location: https://push.example.net/s/LBhhw0OohO-Wl4Oi971UGsB7sdQGUibx`
+	return S({notes},
+		A0(null,
+			code({source, language: "http"}),
+			ssl(4)),
+		A1(null, 
+			T(null, "There they are:")),
+		L(null,
+			LI(null, A2(null, "A way to push stuff in")),
+			LI(null, A3(null, "A means to hear acks")),
+			LI(null, A4(null, "A means to get stuff out"))))
+}()
+
+var s9= (notes)=> {
 	
 }()
 
 render(
 	<Spectacle>
-		<Deck transition={["zoom"]} transitionDuration={288}>
-			{s1}
-			<Slide bgColor="primary" notes="">
-				<Heading size={0.6} caps>
-					Hi. Welcome to
-				</Heading>
-				<Heading caps>
-					WebPush Away.
-				</Heading>
-				<Text>
-					This is a talk on 
-				</Text>
-				<Heading>
-					<Link href="https://tools.ietf.org/html/draft-ietf-webpush-protocol-02">
-						WebPush
-					</Link>
-				</Heading>
-				<Text>
-					<Link href="https://tools.ietf.org/html/draft-ietf-webpush-protocol-02">
- 						draft-ietf-webpush-protocol-02
-					</Link><br/>
-					Generic Event Delivery Using HTTP Push
-				</Text>
-			</Slide>
-			<Slide>
-				<Heading>It's*</Heading>
-				<Heading big>rad</Heading>
-				<Text>*WebPush. But this talk too.</Text>
-				<Text>But not as rad as</Text>
-				<Text>WebPush</Text>
-			</Slide>
-			<Slide>
-				<Heading>Because WebPush is a</Heading>
-				<Heading big>queue</Heading>
-			</Slide>
-			<Slide>
-				<Heading>And queues are</Heading>
-			</Slide>
-			<Slide>
-				<Heading>Great.</Heading>
-			</Slide>
+		<Deck transition={["zoom"]} transitionDuration={288} progress="number">
+			{webpushAway}
+			{rad}
+			{radSpec}
+			{bigQueue}
+			{s4_really}
+			{s5}
+			{s6}
+			{s7}
+			{s8}
+			{s9_created}
 			<Slide>
 				<Heading big>Really great.</Heading>
 			</Slide>
